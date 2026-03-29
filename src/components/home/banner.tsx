7afd4cdd01271913e4ba98b92c68f6006c8dbd34
Sk Shiam Ali <shiam620@gmail.com>
2026-03-30 00:19:22 +0600

    feat: add 3D banner component with animated text and assets

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { Center, Environment, Float, MeshTransmissionMaterial, OrbitControls, Stars, Text3D } from '@react-three/drei'
+import { Center, Environment, Float, Html, MeshTransmissionMaterial, OrbitControls, Stars, Text3D } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import Image from 'next/image';
 import Typewriter from 'typewriter-effect';
 
 export default function Banner() {
@@ -27,21 +28,10 @@ function CanvasFunction() {
         <div className="h-screen w-full text-center">
             <div className="z-50 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-4">
                 <div className="text-mainColor md:block notranslate mt-16 md:mt-24 w-full flex justify-center">
-                    <p className='flex flex-wrap justify-center gap-1 text-xs sm:text-sm tracking-[0.2em] md:tracking-[0.5em]'>Transforming Ideas into <span className="text-primary">
-                        {/* <Typewriter
-                            options={{
-                                strings: [
-                                    "Web Development",
-                                    "Mobile App Development",
-                                    "Custom Software",
-                                    "Game Development",
-                                ],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        /> */}
+                    {/* <p className='flex flex-wrap justify-center gap-1 text-xs sm:text-sm tracking-[0.2em] md:tracking-[0.5em]'>Transforming Ideas into <span className="text-primary">
+                       
                     </span>
-                    </p>
+                    </p> */}
                     {/* <h1 className="text-4xl font-bold">Transforming Ideas into <br className="hidden md:block" />
                         <span className="text-primary">Digital Reality</span>
                     </h1>
@@ -70,8 +60,8 @@ function CanvasFunction() {
                     <OrbitControls enableZoom={false} />
                 </Float>
             </Canvas>
-            <div className="absolute top-1/5 left-1/2 -translate-x-1/2 w-[800px] h-[400px] bg-primary/20 blur-[100px] rounded-full opacity-50 pointer-events-none" />
-            <div className="absolute bottom-1/5 right-1/2 -translate-x-1/2 w-[600px] h-[300px] bg-secondary/20 blur-[80px] rounded-full opacity-30 pointer-events-none" />
+            <div className="absolute top-1/5 left-1/2 -translate-x-1/2 w-[400px] h-[200px] md:w-[800px] md:h-[400px] bg-primary/20 blur-[100px] rounded-full opacity-50 pointer-events-none" />
+            <div className="absolute bottom-1/5 right-1/2 -translate-x-1/2 w-[300px] h-[150px] md:w-[600px] md:h-[300px] bg-secondary/20 blur-[80px] rounded-full opacity-30 pointer-events-none" />
         </div>
     )
 }
@@ -85,6 +75,23 @@ interface TextProps {
     }
 }
 
+// function TextComponent({ config }: TextProps) {
+//     return (
+//         <group>
+//             <Center scale={[1, 1, 1]} position={[0, 0.8, 0]}>
+//                 {/* 
+//                   To render standard HTML components like Next.js Image inside the 3D Canvas,
+//                   we use the <Html> component from @react-three/drei.
+//                 */}
+//                 <Html transform center>
+//                     {/* <div className="select-none pointer-events-none drop-shadow-2xl"> */}
+//                     <Image src="/ssa-l.png" alt="Logo" width={180} height={120} className="rounded-lg object-contain w-24 h-12 md:w-[180px] md:h-[120px] shadow-2xl" />
+//                     {/* </div> */}
+//                 </Html>
+//             </Center>
+//         </group>
+//     )
+// }
 function TextComponent({ config }: TextProps) {
     return (
         <group>
@@ -100,9 +107,7 @@ function TextComponent({ config }: TextProps) {
                     bevelSize={0.02}
                     bevelSegments={10}
                 >
-                    {/* at.future */}
                     SSA
-                    {/* <div className="">ss</div> */}
                     <MeshTransmissionMaterial {...config} />
                 </Text3D>
             </Center>
